6299cbf82d808e3d80a2661868e2227e21419cf8
Алексей Булат <bulat_lesha@mail.ru>
2026-09-24 19:13:21 +0300

    6

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -39,7 +39,7 @@ export default function Home({ loaderData }: Route.ComponentProps) {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div>
-        <div className="title">Docker test app_5</div>
+        <div className="title">Docker test app_6</div>
         {loaderData.ok ? (
           <div className="envTitle">count= {loaderData.count}</div>
         ) : (
